6bd2785401e9eb755342a61bdf9a61d40061c927
Matthew Harvey <matthewtlharvey@gmail.com>
2023-01-18 00:14:31 +0000

    reset to previous functional state

--- api/index.js
+++ api/index.js
@@ -59,7 +59,7 @@ __export(root_exports, {
 var import_react2 = require("@remix-run/react");
 
 // app/styles/app.css
-var app_default = "/build/_assets/app-7OKNAPF6.css";
+var app_default = "/build/_assets/app-OYLN25MT.css";
 
 // app/root.tsx
 var import_jsx_dev_runtime2 = require("react/jsx-dev-runtime");
@@ -136,8 +136,8 @@ async function loader() {
     headers: {
       "X-Auth-Token": (_a = process.env.PUBLIC_FOOTBALL_API_KEY) == null ? void 0 : _a.toString()
     }
-  }, fantasty_stats_return = await (await fetch("https://fantasy.premierleague.com/api/bootstrap-static/")).json(), api_football_return = await (await fetch("https://api.football-data.org/v4/competitions/PL/matches", options)).json();
-  return (0, import_node.json)({ date: new Date(), api_football: api_football_return, fantasy: fantasty_stats_return });
+  }, return_response = await (await fetch("https://api.football-data.org/v4/competitions/PL/scorers", options)).json();
+  return (0, import_node.json)(return_response);
 }
 function Index() {
   let test = (0, import_react3.useLoaderData)();
@@ -146,249 +146,38 @@ function Index() {
     let weights = tf.tensor([2.5, 0.01]);
     return data2.dot(weights);
   }, infectedPeople = [2, 5, 12, 30], infectedCountries = [1, 1, 4, 5], data = tf.tensor([infectedPeople[1], infectedCountries[1]]), prediction = predict(data);
-  return /* @__PURE__ */ (0, import_jsx_dev_runtime3.jsxDEV)(import_jsx_dev_runtime3.Fragment, { children: [
-    /* @__PURE__ */ (0, import_jsx_dev_runtime3.jsxDEV)("div", { className: "navbar bg-base-100 sticky top-0", children: [
-      /* @__PURE__ */ (0, import_jsx_dev_runtime3.jsxDEV)("div", { className: "navbar-start", children: [
-        /* @__PURE__ */ (0, import_jsx_dev_runtime3.jsxDEV)("div", { className: "dropdown", children: [
-          /* @__PURE__ */ (0, import_jsx_dev_runtime3.jsxDEV)("label", { tabIndex: 0, className: "btn btn-ghost lg:hidden", children: /* @__PURE__ */ (0, import_jsx_dev_runtime3.jsxDEV)("svg", { xmlns: "http://www.w3.org/2000/svg", className: "h-5 w-5", fill: "none", viewBox: "0 0 24 24", stroke: "currentColor", children: /* @__PURE__ */ (0, import_jsx_dev_runtime3.jsxDEV)("path", { strokeLinecap: "round", strokeLinejoin: "round", strokeWidth: "2", d: "M4 6h16M4 12h8m-8 6h16" }, void 0, !1, {
-            fileName: "app/routes/index.tsx",
-            lineNumber: 43,
-            columnNumber: 129
-          }, this) }, void 0, !1, {
-            fileName: "app/routes/index.tsx",
-            lineNumber: 43,
-            columnNumber: 15
-          }, this) }, void 0, !1, {
-            fileName: "app/routes/index.tsx",
-            lineNumber: 42,
-            columnNumber: 13
-          }, this),
-          /* @__PURE__ */ (0, import_jsx_dev_runtime3.jsxDEV)("ul", { tabIndex: 0, className: "menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52", children: [
-            /* @__PURE__ */ (0, import_jsx_dev_runtime3.jsxDEV)("li", { children: /* @__PURE__ */ (0, import_jsx_dev_runtime3.jsxDEV)("a", { children: "Item 1" }, void 0, !1, {
-              fileName: "app/routes/index.tsx",
-              lineNumber: 46,
-              columnNumber: 19
-            }, this) }, void 0, !1, {
-              fileName: "app/routes/index.tsx",
-              lineNumber: 46,
-              columnNumber: 15
-            }, this),
-            /* @__PURE__ */ (0, import_jsx_dev_runtime3.jsxDEV)("li", { tabIndex: 0, children: [
-              /* @__PURE__ */ (0, import_jsx_dev_runtime3.jsxDEV)("a", { className: "justify-between", children: [
-                "Parent",
-                /* @__PURE__ */ (0, import_jsx_dev_runtime3.jsxDEV)("svg", { className: "fill-current", xmlns: "http://www.w3.org/2000/svg", width: "24", height: "24", viewBox: "0 0 24 24", children: /* @__PURE__ */ (0, import_jsx_dev_runtime3.jsxDEV)("path", { d: "M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" }, void 0, !1, {
-                  fileName: "app/routes/index.tsx",
-                  lineNumber: 50,
-                  columnNumber: 127
-                }, this) }, void 0, !1, {
-                  fileName: "app/routes/index.tsx",
-                  lineNumber: 50,
-                  columnNumber: 19
-                }, this)
-              ] }, void 0, !0, {
-                fileName: "app/routes/index.tsx",
-                lineNumber: 48,
-                columnNumber: 17
-              }, this),
-              /* @__PURE__ */ (0, import_jsx_dev_runtime3.jsxDEV)("ul", { className: "p-2", children: [
-                /* @__PURE__ */ (0, import_jsx_dev_runtime3.jsxDEV)("li", { children: /* @__PURE__ */ (0, import_jsx_dev_runtime3.jsxDEV)("a", { children: "Submenu 1" }, void 0, !1, {
-                  fileName: "app/routes/index.tsx",
-                  lineNumber: 53,
-                  columnNumber: 23
-                }, this) }, void 0, !1, {
-                  fileName: "app/routes/index.tsx",
-                  lineNumber: 53,
-                  columnNumber: 19
-                }, this),
-                /* @__PURE__ */ (0, import_jsx_dev_runtime3.jsxDEV)("li", { children: /* @__PURE__ */ (0, import_jsx_dev_runtime3.jsxDEV)("a", { children: "Submenu 2" }, void 0, !1, {
-                  fileName: "app/routes/index.tsx",
-                  lineNumber: 54,
-                  columnNumber: 23
-                }, this) }, void 0, !1, {
-                  fileName: "app/routes/index.tsx",
-                  lineNumber: 54,
-                  columnNumber: 19
-                }, this)
-              ] }, void 0, !0, {
-                fileName: "app/routes/index.tsx",
-                lineNumber: 52,
-                columnNumber: 17
-              }, this)
-            ] }, void 0, !0, {
-              fileName: "app/routes/index.tsx",
-              lineNumber: 47,
-              columnNumber: 15
-            }, this),
-            /* @__PURE__ */ (0, import_jsx_dev_runtime3.jsxDEV)("li", { children: /* @__PURE__ */ (0, import_jsx_dev_runtime3.jsxDEV)("a", { children: "Item 3" }, void 0, !1, {
-              fileName: "app/routes/index.tsx",
-              lineNumber: 57,
-              columnNumber: 19
-            }, this) }, void 0, !1, {
-              fileName: "app/routes/index.tsx",
-              lineNumber: 57,
-              columnNumber: 15
-            }, this)
-          ] }, void 0, !0, {
-            fileName: "app/routes/index.tsx",
-            lineNumber: 45,
-            columnNumber: 13
-          }, this)
-        ] }, void 0, !0, {
-          fileName: "app/routes/index.tsx",
-          lineNumber: 41,
-          columnNumber: 11
-        }, this),
-        /* @__PURE__ */ (0, import_jsx_dev_runtime3.jsxDEV)("a", { className: "btn btn-ghost normal-case text-xl", children: "premPredict" }, void 0, !1, {
-          fileName: "app/routes/index.tsx",
-          lineNumber: 60,
-          columnNumber: 11
-        }, this)
-      ] }, void 0, !0, {
-        fileName: "app/routes/index.tsx",
-        lineNumber: 40,
-        columnNumber: 9
-      }, this),
-      /* @__PURE__ */ (0, import_jsx_dev_runtime3.jsxDEV)("div", { className: "navbar-center hidden lg:flex", children: /* @__PURE__ */ (0, import_jsx_dev_runtime3.jsxDEV)("ul", { className: "menu menu-horizontal px-1", children: [
-        /* @__PURE__ */ (0, import_jsx_dev_runtime3.jsxDEV)("li", { children: /* @__PURE__ */ (0, import_jsx_dev_runtime3.jsxDEV)("a", { children: "Item 1" }, void 0, !1, {
-          fileName: "app/routes/index.tsx",
-          lineNumber: 64,
-          columnNumber: 17
-        }, this) }, void 0, !1, {
-          fileName: "app/routes/index.tsx",
-          lineNumber: 64,
-          columnNumber: 13
-        }, this),
-        /* @__PURE__ */ (0, import_jsx_dev_runtime3.jsxDEV)("li", { tabIndex: 0, children: [
-          /* @__PURE__ */ (0, import_jsx_dev_runtime3.jsxDEV)("a", { children: [
-            "Parent",
-            /* @__PURE__ */ (0, import_jsx_dev_runtime3.jsxDEV)("svg", { className: "fill-current", xmlns: "http://www.w3.org/2000/svg", width: "20", height: "20", viewBox: "0 0 24 24", children: /* @__PURE__ */ (0, import_jsx_dev_runtime3.jsxDEV)("path", { d: "M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" }, void 0, !1, {
-              fileName: "app/routes/index.tsx",
-              lineNumber: 68,
-              columnNumber: 125
-            }, this) }, void 0, !1, {
-              fileName: "app/routes/index.tsx",
-              lineNumber: 68,
-              columnNumber: 17
-            }, this)
-          ] }, void 0, !0, {
-            fileName: "app/routes/index.tsx",
-            lineNumber: 66,
-            columnNumber: 15
-          }, this),
-          /* @__PURE__ */ (0, import_jsx_dev_runtime3.jsxDEV)("ul", { className: "p-2", children: [
-            /* @__PURE__ */ (0, import_jsx_dev_runtime3.jsxDEV)("li", { children: /* @__PURE__ */ (0, import_jsx_dev_runtime3.jsxDEV)("a", { children: "Submenu 1" }, void 0, !1, {
-              fileName: "app/routes/index.tsx",
-              lineNumber: 71,
-              columnNumber: 21
-            }, this) }, void 0, !1, {
-              fileName: "app/routes/index.tsx",
-              lineNumber: 71,
-              columnNumber: 17
-            }, this),
-            /* @__PURE__ */ (0, import_jsx_dev_runtime3.jsxDEV)("li", { children: /* @__PURE__ */ (0, import_jsx_dev_runtime3.jsxDEV)("a", { children: "Submenu 2" }, void 0, !1, {
-              fileName: "app/routes/index.tsx",
-              lineNumber: 72,
-              columnNumber: 21
-            }, this) }, void 0, !1, {
-              fileName: "app/routes/index.tsx",
-              lineNumber: 72,
-              columnNumber: 17
-            }, this)
-          ] }, void 0, !0, {
-            fileName: "app/routes/index.tsx",
-            lineNumber: 70,
-            columnNumber: 15
-          }, this)
-        ] }, void 0, !0, {
-          fileName: "app/routes/index.tsx",
-          lineNumber: 65,
-          columnNumber: 13
-        }, this),
-        /* @__PURE__ */ (0, import_jsx_dev_runtime3.jsxDEV)("li", { children: /* @__PURE__ */ (0, import_jsx_dev_runtime3.jsxDEV)("a", { children: "Item 3" }, void 0, !1, {
-          fileName: "app/routes/index.tsx",
-          lineNumber: 75,
-          columnNumber: 17
-        }, this) }, void 0, !1, {
-          fileName: "app/routes/index.tsx",
-          lineNumber: 75,
-          columnNumber: 13
-        }, this)
-      ] }, void 0, !0, {
-        fileName: "app/routes/index.tsx",
-        lineNumber: 63,
-        columnNumber: 11
-      }, this) }, void 0, !1, {
-        fileName: "app/routes/index.tsx",
-        lineNumber: 62,
-        columnNumber: 9
-      }, this),
-      /* @__PURE__ */ (0, import_jsx_dev_runtime3.jsxDEV)("div", { className: "navbar-end", children: /* @__PURE__ */ (0, import_jsx_dev_runtime3.jsxDEV)("a", { className: "btn", children: "Get started" }, void 0, !1, {
-        fileName: "app/routes/index.tsx",
-        lineNumber: 79,
-        columnNumber: 11
-      }, this) }, void 0, !1, {
-        fileName: "app/routes/index.tsx",
-        lineNumber: 78,
-        columnNumber: 9
-      }, this)
-    ] }, void 0, !0, {
+  return /* @__PURE__ */ (0, import_jsx_dev_runtime3.jsxDEV)("div", { style: { fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }, children: [
+    /* @__PURE__ */ (0, import_jsx_dev_runtime3.jsxDEV)("h1", { className: "font-bold", children: "Welcome to Remix" }, void 0, !1, {
       fileName: "app/routes/index.tsx",
-      lineNumber: 39,
+      lineNumber: 36,
       columnNumber: 7
     }, this),
-    /* @__PURE__ */ (0, import_jsx_dev_runtime3.jsxDEV)("div", { className: "hero bg-base-200 my-20", children: /* @__PURE__ */ (0, import_jsx_dev_runtime3.jsxDEV)("div", { className: "hero-content text-center", children: /* @__PURE__ */ (0, import_jsx_dev_runtime3.jsxDEV)("div", { className: "max-w-md", children: [
-      /* @__PURE__ */ (0, import_jsx_dev_runtime3.jsxDEV)("h1", { className: "text-5xl font-bold", children: [
-        test.api_football.competition.name,
-        " Predictions"
-      ] }, void 0, !0, {
-        fileName: "app/routes/index.tsx",
-        lineNumber: 85,
-        columnNumber: 13
-      }, this),
-      /* @__PURE__ */ (0, import_jsx_dev_runtime3.jsxDEV)("p", { className: "py-6", children: [
-        "Predicted next day infections... infected people: ",
-        data.dataSync()[0],
-        ", infected countries ",
-        data.dataSync()[1],
-        ", prediction: ",
-        prediction.dataSync()[0]
-      ] }, void 0, !0, {
-        fileName: "app/routes/index.tsx",
-        lineNumber: 86,
-        columnNumber: 13
-      }, this),
-      /* @__PURE__ */ (0, import_jsx_dev_runtime3.jsxDEV)("button", { className: "btn btn-primary", children: "Get Started" }, void 0, !1, {
-        fileName: "app/routes/index.tsx",
-        lineNumber: 87,
-        columnNumber: 13
-      }, this),
-      /* @__PURE__ */ (0, import_jsx_dev_runtime3.jsxDEV)("img", { src: test.api_football.competition.emblem, alt: "League Emblem", className: "m-auto justify-center" }, void 0, !1, {
-        fileName: "app/routes/index.tsx",
-        lineNumber: 88,
-        columnNumber: 13
-      }, this)
+    /* @__PURE__ */ (0, import_jsx_dev_runtime3.jsxDEV)("p", { children: [
+      "Predicted next day infections... infected people: ",
+      data.dataSync()[0],
+      ", infected countries ",
+      data.dataSync()[1],
+      ", prediction: ",
+      prediction.dataSync()[0]
     ] }, void 0, !0, {
       fileName: "app/routes/index.tsx",
-      lineNumber: 84,
-      columnNumber: 11
-    }, this) }, void 0, !1, {
+      lineNumber: 37,
+      columnNumber: 7
+    }, this),
+    /* @__PURE__ */ (0, import_jsx_dev_runtime3.jsxDEV)("p", { children: test.competition.name }, void 0, !1, {
       fileName: "app/routes/index.tsx",
-      lineNumber: 83,
-      columnNumber: 9
-    }, this) }, void 0, !1, {
-      fileName: "app/routes/index.tsx",
-      lineNumber: 82,
+      lineNumber: 40,
       columnNumber: 7
     }, this)
   ] }, void 0, !0, {
     fileName: "app/routes/index.tsx",
-    lineNumber: 38,
+    lineNumber: 35,
     columnNumber: 5
   }, this);
 }
 
 // server-assets-manifest:@remix-run/dev/assets-manifest
-var assets_manifest_default = { version: "d91733be", entry: { module: "/build/entry.client-XCHZLFYJ.js", imports: ["/build/_shared/chunk-N4RF36SG.js"] }, routes: { root: { id: "root", parentId: void 0, path: "", index: void 0, caseSensitive: void 0, module: "/build/root-E5IADX6R.js", imports: void 0, hasAction: !1, hasLoader: !1, hasCatchBoundary: !1, hasErrorBoundary: !1 }, "routes/index": { id: "routes/index", parentId: "root", path: void 0, index: !0, caseSensitive: void 0, module: "/build/routes/index-GUWI6ZS4.js", imports: void 0, hasAction: !1, hasLoader: !0, hasCatchBoundary: !1, hasErrorBoundary: !1 } }, url: "/build/manifest-D91733BE.js" };
+var assets_manifest_default = { version: "bf1802db", entry: { module: "/build/entry.client-XCHZLFYJ.js", imports: ["/build/_shared/chunk-N4RF36SG.js"] }, routes: { root: { id: "root", parentId: void 0, path: "", index: void 0, caseSensitive: void 0, module: "/build/root-4TQ6DWYI.js", imports: void 0, hasAction: !1, hasLoader: !1, hasCatchBoundary: !1, hasErrorBoundary: !1 }, "routes/index": { id: "routes/index", parentId: "root", path: void 0, index: !0, caseSensitive: void 0, module: "/build/routes/index-SLGGC2UV.js", imports: void 0, hasAction: !1, hasLoader: !0, hasCatchBoundary: !1, hasErrorBoundary: !1 } }, url: "/build/manifest-BF1802DB.js" };
 
 // server-entry-module:@remix-run/dev/server-build
 var assetsBuildDirectory = "public\\build", future = { v2_meta: !1 }, publicPath = "/build/", entry = { module: entry_server_exports }, routes = {
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
 
 import { json } from "@remix-run/node"; // or cloudflare/deno
 import { useLoaderData } from "@remix-run/react";
@@ -12,16 +11,14 @@ export async function loader() {
       'X-Auth-Token': api_key,
     }
   };
-  let fantasty_stats = await fetch('https://fantasy.premierleague.com/api/bootstrap-static/');
-  const fantasty_stats_return = await fantasty_stats.json();
   // @ts-ignore
-  let api_football = await fetch('https://api.football-data.org/v4/competitions/PL/matches', options);
-  const api_football_return = await api_football.json();
-  return json({ date: new Date(), api_football: api_football_return, fantasy: fantasty_stats_return});
+  const response = await fetch('https://api.football-data.org/v4/competitions/PL/scorers', options);
+  const return_response = await response.json();
+  return json(return_response);
 }
 
 export default function Index() {
-  const test = useLoaderData<typeof loader>();
+  const test = useLoaderData();
   console.log(test);
 
   const predict = (data: tf.Tensor<tf.Rank>) => {
@@ -35,60 +32,12 @@ export default function Index() {
   const prediction = predict(data)
   
   return (
-    <>
-      <div className="navbar bg-base-100 sticky top-0">
-        <div className="navbar-start">
-          <div className="dropdown">
-            <label tabIndex={0} className="btn btn-ghost lg:hidden">
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-            </label>
-            <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-              <li><a>Item 1</a></li>
-              <li tabIndex={0}>
-                <a className="justify-between">
-                  Parent
-                  <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z"/></svg>
-                </a>
-                <ul className="p-2">
-                  <li><a>Submenu 1</a></li>
-                  <li><a>Submenu 2</a></li>
-                </ul>
-              </li>
-              <li><a>Item 3</a></li>
-            </ul>
-          </div>
-          <a className="btn btn-ghost normal-case text-xl">premPredict</a>
-        </div>
-        <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">
-            <li><a>Item 1</a></li>
-            <li tabIndex={0}>
-              <a>
-                Parent
-                <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
-              </a>
-              <ul className="p-2">
-                <li><a>Submenu 1</a></li>
-                <li><a>Submenu 2</a></li>
-              </ul>
-            </li>
-            <li><a>Item 3</a></li>
-          </ul>
-        </div>
-        <div className="navbar-end">
-          <a className="btn">Get started</a>
-        </div>
-      </div>
-      <div className="hero bg-base-200 my-20">
-        <div className="hero-content text-center">
-          <div className="max-w-md">
-            <img src={test.api_football.competition.emblem} alt="League Emblem" className="m-auto justify-center" />
-            <h1 className="text-5xl font-bold">{test.api_football.competition.name} Predictions</h1>
-            <p className="py-6">Predicted next day infections... infected people: {data.dataSync()[0]}, infected countries {data.dataSync()[1]}, prediction: {prediction.dataSync()[0]}</p>
-            <button className="btn btn-primary">Get Started</button>
-          </div>
-        </div>
-      </div>
-    </>
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+      <h1 className="font-bold">Welcome to Remix</h1>
+      <p>
+        Predicted next day infections... infected people: {data.dataSync()[0]}, infected countries {data.dataSync()[1]}, prediction: {prediction.dataSync()[0]}
+      </p>
+      <p>{test.competition.name}</p>
+    </div>
   );
 }
