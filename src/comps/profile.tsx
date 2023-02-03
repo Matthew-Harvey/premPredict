0bbc03b62c4270731e281ab5558cf33b7e9dbf7e
Matthew Harvey <matthewtlharvey@gmail.com>
2023-02-03 00:42:23 +0000

    comp profile import update

--- src/comps/profile.tsx
+++ src/comps/profile.tsx
@@ -1,15 +1,16 @@
+/* eslint-disable @next/next/no-img-element */
+import { useUser } from "@auth0/nextjs-auth0/client";
 import React from "react";
-import { useAuth0 } from "@auth0/auth0-react";
 
 const Profile = () => {
-  const { user, isAuthenticated, isLoading } = useAuth0();
+  const { user, isLoading } = useUser();
 
   if (isLoading) {
     return <div>Loading ...</div>;
   }
 
   return (
-    isAuthenticated && (
+    user && (
       <div>
         <img src={user?.picture} alt={user?.name} />
         <h2>{user?.name}</h2>
